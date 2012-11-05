c212f4066626901b8456dcdfb197c86071135092
Geert Smelt <gasmelt@student.ru.nl>
2012-11-05 16:10:32 +0100

    Added the `select()` function which's absence caused problems selecting the applet on the javacard

--- Card/src/card/Card.java
+++ Card/src/card/Card.java
@@ -6,9 +6,9 @@ import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 
 /**
- * @author Geert Smelt (0609838)
- * @author Robin Oostrum (0609803)
  *
+ * @author Geert Smelt
+ * @author Robin Oostrum
  */
 public class Card extends Applet implements ISO7816 {
 
@@ -18,11 +18,14 @@ public class Card extends Applet implements ISO7816 {
 	}
 
 	public void process(APDU apdu) {
-		// Good practice: Return 9000 on SELECT
+		// Ignore the CommandAPDU that selects this applet on the card
 		if (selectingApplet()) {
 			return;
 		}
-
+		
+		/*
+		 * The actual work happens here when a CommandAPDU is received by the card. 
+		 */
 		byte[] buf = apdu.getBuffer();
 		switch (buf[ISO7816.OFFSET_INS]) {
 		case (byte) 0x00:
@@ -31,6 +34,17 @@ public class Card extends Applet implements ISO7816 {
 			// good practice: If you don't know the INStruction, say so:
 			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
+	}
+	
+	public boolean select () {
+		/*
+		 * After a lot of trial-and-error I found out that this function is responsible for successful selection.
+		 * If you return true the status word will be 0x9000
+		 * If you return false the status word will be 0x6999
+		 * 
+		 * Good practice: Return 9000 on SELECT
+		 */
+		return true;
 	}
 
 }
